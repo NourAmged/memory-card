aec1f3fbfb14a7a291b327078c174a627d9bc732
Nour Amged <nouramgged2005@gmail.com>
2025-08-06 19:02:55 +0300

    started working of fetching the data

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return (
+        <div className="card">
+            <p id='bird-name'>yellow headed bird</p>
+        </div>
+    );
+}
+
+export default Card;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,49 @@
 
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
 function Main() {
+    const [birdData, setBirdData] = useState(null);
+
+
+    useEffect(() => {
+        fetch('https://nuthatch.lastelm.software/v2/birds?family=Troglodytidae&hasImg=true', {
+            headers: {
+                'api-key': `${import.meta.env.VITE_API_KEY}`
+            }
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not OK');
+                }
+                return response.text(); 
+            })
+            .then((text) => {
+                try {
+                    const data = JSON.parse(text);
+                    setBirdData(data);
+                } catch (err) {
+                    console.error('Manual JSON parse failed:', err);
+                }
+            })
+            .catch((error) => {
+                console.error('Fetch error:', error);
+            });
+    }, []);
+
+
     return (
         <div className="main-content">
             <div className="card-container">
                 <div className="first-row">
-                    <div className="card">
-                        <p id='bird-name'>yellow headed bird</p>
-                    </div>
-                    <div className="card">
-                        <p id='bird-name'>yellow headed bird</p>
-                    </div>
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
                 <div className="second-row">
-                    <div className="card">
-                        <p id='bird-name'>yellow headed bird</p>
-                    </div>
-                    <div className="card">
-                        <p id='bird-name'>yellow headed bird</p>
-                    </div>
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
             </div>
         </div>
